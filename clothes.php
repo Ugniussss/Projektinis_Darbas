82e9048432e6eae538839473f8b6a976475babae
Ugniusss <gnsaura@gmail.com>
2019-10-06 22:51:15 +0300

    4 commitas: Na iki rytojaus paskutinis commitas, šiandien turėjau didesnių tikslų, bet įsiskaičius į užduotį sustojau daryti ir supratau, kad užduotis reikalauja darbo su front-endu. Įlindęs į Vue.js aplinką supratau kiek ten daug reikalų yra, ir tam skyriau laiko. Dirbsiu toliau prie šio projekto.

---
+++ clothes.php
@@ -0,0 +1,31 @@
+<?php
+
+require "header.php";
+
+?>
+<body>
+    <section class="main-section-style">'
+        <ul class ="menu-style">
+            <li>
+                <a href="Pages/clothes.php">
+                    <span> Drabužiai</span>
+                </a>
+            </li>
+            <li>
+                <a href="boots.php">
+                    <span>Batai</span>
+                </a>
+            </li>
+        </ul>
+        <p class="clothes">
+            <a href="clothes-1.php">
+                <img src="" alt="NIKE WILD RUNNERS">
+            </a>
+        </p
+    </section>
+</body>
+<?php
+require "footer.php";
+?>
+
+
---
+++ signupsuccesful.php
@@ -0,0 +1,23 @@
+<?php
+    require "header.php";
+
+    ?>
+
+
+
+<section class="main-section-style">
+    <h2>Jūs sėkmingai užsiregistravote! Prašome prisijungti </h2>
+    <div class="main-style">
+        <form class="signup-style" action="index.php" method="POST">
+      <button type="submit" name="submit">Grįžti atgal </button>
+        </form>
+    </div>
+</section>
+
+
+
+<?php
+    require "footer.php";
+
+
+    ?>
